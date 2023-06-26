2596269484196e877c16da0507a4c725266b4ce6
Talha Mustafa <talhamustafa@pk.folio3.com>
2023-06-25 23:29:37 -0700

    first commitssd

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Link to={"app"}>Hello world!</Link>,
+    element: <Link to={"app"}>Hello world app</Link>,
   },
   {
     path: "/app",
